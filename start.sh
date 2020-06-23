e9dd073948bbeadabf2b3e9868d1c9afe2f2e476
Einar Lönn <el@sunet.se>
2020-06-23 12:47:50 +0200

    So the influxdb-user needs ownership of the certificate? Investigating

--- start.sh
+++ start.sh
@@ -1,13 +1,20 @@
 #!/bin/sh -x
 
+# Some variables
+CERTDIR="/etc/dehydrated/certs"
+FQDN="influx-1.sunet.se"
+
 # Influxv2 is special and DEMANDS a home directory to run from or nothing works as intended, oh and you cant run as root either (at least "restore" will not work)
 mkdir -p /home/influxdb
 chown -R influxdb:influxdb /home/influxdb
 
+# Not sure why but I need ownership of certificate & key or influx cannot start(!?)
+chown influxdb:influxdb ${CERTDIR}/${FQDN}.*
+
 # Drop the backup of your database somewhere (remember to have the influxdb-user on the system and owning all files) then restore with below
 runuser influxdb -c "/opt/local/bin/influxd restore --backup-path /home/influxdb/backup/today"
 
-FLAGS="--http-bind-address=0.0.0.0:9999 --log-level debug --reporting-disabled --tls-cert=/etc/dehydrated/certs/influx-1.sunet.se.crt --tls-key=/etc/dehydrated/certs/influx-1.sunet.se.key"
+FLAGS="--http-bind-address=0.0.0.0:9999 --log-level debug --reporting-disabled --tls-cert=${CERTDIR}/${FQDN}.crt --tls-key=${CERTDIR}/${FQDN}.key"
 # FIXME: I'd want it to run with something similar to below but it doesnt work as intended, you can run it wherever you like... as long as it's in a users home directory 
 #FLAGS="--bolt-path ${INFLUXDB}.influxdbv2/influxd.bolt --engine-path ${INFLUXDB}.influxdbv2/engine --http-bind-address 127.0.0.1:9999 --log-level debug --reporting-disabled"
 
