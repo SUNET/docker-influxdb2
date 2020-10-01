6e3268470ad955d7308e7a139afed18ce4b4bbc3
Einar Lönn <el@sunet.se>
2020-10-01 15:00:17 +0200

    Not pretty but could work, in theory

--- start.sh
+++ start.sh
@@ -11,5 +11,11 @@ FLAGS="--http-bind-address=0.0.0.0:9999 --log-level debug --reporting-disabled"
 # FIXME: I'd want it to run with something similar to below but it doesnt work as intended, you can run it wherever you like... as long as it's in a users home directory 
 #FLAGS="--bolt-path ${INFLUXDB}.influxdbv2/influxd.bolt --engine-path ${INFLUXDB}.influxdbv2/engine --http-bind-address 127.0.0.1:9999 --log-level debug --reporting-disabled"
 
+### FIXME: This is just an ugly hack to try and migrate the database from influx2-beta to influx2-rc
+mv /home/influxdb/.influxdbv2 /home/influxdb/.influxdbv2_old
+FLAGS="--bolt-path /home/influxdb/.influxdbv2_old/influxd.bolt --engine-path /home/influxdb/.influxdbv2_old/engine --http-bind-address=0.0.0.0:9999 --log-level debug --reporting-disabled "
+runuser influxdb -c "/opt/local/bin/influxd_old $FLAGS &"
+runuser influxdb -c "/opt/local/bin/influxd --http-bind-address=0.0.0.0:8046 --log-level debug --reporting-disabled"
+
 # FIXME: I'm not entirely sure why but whatever you do you'll end up with 2 databases, one in a semi-proper place (/var/lib/influxdb/.influxdbv2) and one in a static place (~/.influxdbv2).
-runuser influxdb -c "/opt/local/bin/influxd $FLAGS "
+#runuser influxdb -c "/opt/local/bin/influxd $FLAGS "
