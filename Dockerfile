d929c5ea45cc7201af9e6b2478bca1f6b5f30b79
Einar Lönn <el@sunet.se>
2020-10-28 10:43:45 +0100

    Disable everything for a cleanup later

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu:20.10
 
 RUN apt-get update && apt-get install -y --no-install-recommends wget
 
-RUN wget --quiet --no-check-certificate https://dl.influxdata.com/influxdb/releases/influxdb-2.0.0-rc.0_linux_amd64.tar.gz
+RUN wget --quiet --no-check-certificate https://dl.influxdata.com/influxdb/releases/influxdb-2.0.0-rc.2_linux_amd64.tar.gz
 RUN tar xvfz influxdb-2.*linux_amd64.tar.gz
 RUN mkdir -p /opt/local/bin 
 RUN mv -v influ*/influ* /opt/local/bin/
@@ -12,12 +12,12 @@ RUN chmod a+rx /start.sh
 ### These lines below are for reasons of migration from ALPHA/BETA influx2 into RC influx2. Can (probably) be disabled any day now.
 ## The new binaries MUST be disabled and not started or Docker will exit due to non-backwardscompat database
 # So in theory everything above SHOULD work after the migration has been done, the rows below are for future reference/use for migration
-RUN wget --quiet --no-check-certificate https://dl.influxdata.com/influxdb/releases/influxdb_2.0.0-beta.10_linux_amd64.tar.gz
-RUN tar xvzf influxdb_2.0.0-beta.10_linux_amd64.tar.gz
-RUN mv /opt/local/bin/influxd /opt/local/bin/influxd_new
-RUN mv /opt/local/bin/influx /opt/local/bin/influx_new
-RUN mv -v influ*/influ* /opt/local/bin/
-EXPOSE 8046
+#RUN wget --quiet --no-check-certificate https://dl.influxdata.com/influxdb/releases/influxdb_2.0.0-beta.10_linux_amd64.tar.gz
+#RUN tar xvzf influxdb_2.0.0-beta.10_linux_amd64.tar.gz
+#RUN mv /opt/local/bin/influxd /opt/local/bin/influxd_new
+#RUN mv /opt/local/bin/influx /opt/local/bin/influx_new
+#RUN mv -v influ*/influ* /opt/local/bin/
+#EXPOSE 8046
 ### END OF CRAZY MIGRATION
 
 EXPOSE 9999
--- start.sh
+++ start.sh
@@ -1,21 +1,21 @@
 #!/bin/sh -x
 
 # Influxv2 is special and DEMANDS a home directory to run from or nothing works as intended, oh and you cant run as root either (at least "restore" will not work)
-mkdir -p /home/influxdb
-chown -R influxdb:influxdb /home/influxdb
+#mkdir -p /home/influxdb
+#chown -R influxdb:influxdb /var/lib/influxdb
 
 # Drop the backup of your database somewhere (remember to have the influxdb-user on the system and owning all files) then restore with below
-runuser influxdb -c "/opt/local/bin/influxd restore --backup-path /home/influxdb/backup/today"
+#runuser influxdb -c "/opt/local/bin/influxd restore --backup-path /home/influxdb/backup/today"
 
 FLAGS="--http-bind-address=0.0.0.0:9999 --log-level debug --reporting-disabled"
 # FIXME: I'd want it to run with something similar to below but it doesnt work as intended, you can run it wherever you like... as long as it's in a users home directory 
 #FLAGS="--bolt-path ${INFLUXDB}.influxdbv2/influxd.bolt --engine-path ${INFLUXDB}.influxdbv2/engine --http-bind-address 127.0.0.1:9999 --log-level debug --reporting-disabled"
 
 ### FIXME: This is just an ugly hack to try and migrate the database from influx2-beta to influx2-rc
-mv /home/influxdb/.influxdbv2 /home/influxdb/.influxdbv2_old
-FLAGS="--bolt-path /home/influxdb/.influxdbv2_old/influxd.bolt --engine-path /home/influxdb/.influxdbv2_old/engine --http-bind-address=0.0.0.0:9999 --log-level debug --reporting-disabled "
-runuser influxdb -c "/opt/local/bin/influxd_old $FLAGS &"
-runuser influxdb -c "/opt/local/bin/influxd --http-bind-address=0.0.0.0:8046 --log-level debug --reporting-disabled"
+#mv /home/influxdb/.influxdbv2 /home/influxdb/.influxdbv2_old
+#FLAGS="--bolt-path /home/influxdb/.influxdbv2_old/influxd.bolt --engine-path /home/influxdb/.influxdbv2_old/engine --http-bind-address=0.0.0.0:9999 --log-level debug --reporting-disabled "
+#runuser influxdb -c "/opt/local/bin/influxd_old $FLAGS &"
+#runuser influxdb -c "/opt/local/bin/influxd --http-bind-address=0.0.0.0:8046 --log-level debug --reporting-disabled"
 
 # FIXME: I'm not entirely sure why but whatever you do you'll end up with 2 databases, one in a semi-proper place (/var/lib/influxdb/.influxdbv2) and one in a static place (~/.influxdbv2).
 #runuser influxdb -c "/opt/local/bin/influxd $FLAGS "
