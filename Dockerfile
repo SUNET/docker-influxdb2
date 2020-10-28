20c20e4308785e48db27b5d6d9a206d7815ce7f5
Einar Lönn <el@sunet.se>
2020-10-28 11:36:05 +0100

    Something like this is what I'd like to run, let's see if influx2 can eat it now with the RC2 out

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,7 @@ RUN apt-get update && apt-get install -y --no-install-recommends wget
 
 RUN wget --quiet --no-check-certificate https://dl.influxdata.com/influxdb/releases/influxdb-2.0.0-rc.2_linux_amd64.tar.gz
 RUN tar xvfz influxdb-2.*linux_amd64.tar.gz
-RUN mkdir -p /opt/local/bin 
-RUN mv -v influ*/influ* /opt/local/bin/
+RUN mv -v influ*/influ* /usr/local/bin/
 COPY start.sh /
 RUN chmod a+rx /start.sh
 
--- start.sh
+++ start.sh
@@ -3,13 +3,16 @@
 # Influxv2 is special and DEMANDS a home directory to run from or nothing works as intended, oh and you cant run as root either (at least "restore" will not work)
 #mkdir -p /home/influxdb
 #chown -R influxdb:influxdb /var/lib/influxdb
+INFLUXDB="/var/lib/influxdb/"
 
 # Drop the backup of your database somewhere (remember to have the influxdb-user on the system and owning all files) then restore with below
 #runuser influxdb -c "/opt/local/bin/influxd restore --backup-path /home/influxdb/backup/today"
 
-FLAGS="--http-bind-address=0.0.0.0:9999 --log-level debug --reporting-disabled"
+#FLAGS="--http-bind-address=0.0.0.0:9999 --log-level debug --reporting-disabled"
 # FIXME: I'd want it to run with something similar to below but it doesnt work as intended, you can run it wherever you like... as long as it's in a users home directory 
-#FLAGS="--bolt-path ${INFLUXDB}.influxdbv2/influxd.bolt --engine-path ${INFLUXDB}.influxdbv2/engine --http-bind-address 127.0.0.1:9999 --log-level debug --reporting-disabled"
+FLAGS="--bolt-path ${INFLUXDB}.influxdbv2/influxd.bolt --engine-path ${INFLUXDB}.influxdbv2/engine --http-bind-address 127.0.0.1:9999 --log-level debug --reporting-disabled"
+/usr/local/bin/influxdb $FLAGS
+
 
 ### FIXME: This is just an ugly hack to try and migrate the database from influx2-beta to influx2-rc
 #mv /home/influxdb/.influxdbv2 /home/influxdb/.influxdbv2_old
