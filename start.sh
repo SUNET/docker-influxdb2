c7652d1df2a0399aeaac2d0313a42cb249bb644a
Einar Lönn <el@sunet.se>
2020-11-03 14:45:42 +0100

    Let's see if influx2 now can handle certificates

--- start.sh
+++ start.sh
@@ -1,8 +1,16 @@
 #!/bin/sh -x
 
-# Influxv2 is special and DEMANDS a home directory to run from or nothing works as intended, oh and you cant run as root either (at least "restore" will not work)
-#mkdir -p /home/influxdb
-#chown -R influxdb:influxdb /var/lib/influxdb
+printenv
+
+if [ "x${HOSTNAME}" = "x" ]; then
+   HOSTNAME="`hostname`"
+fi
+
+if [ "x${CERTDIR}" = "x" ]; then
+   CERTDIR="/etc/dehydrated/certs"
+fi
+
+# With influx2 out of alpha/beta you can now run in a proper location, some artifacts remain of "/home/influx" but they will be removed
 INFLUXDB="/var/lib/influxdb/"
 
 # Drop the backup of your database somewhere (remember to have the influxdb-user on the system and owning all files) then restore with below
@@ -10,7 +18,7 @@ INFLUXDB="/var/lib/influxdb/"
 
 #FLAGS="--http-bind-address=0.0.0.0:9999 --log-level debug --reporting-disabled"
 # FIXME: I'd want it to run with something similar to below but it doesnt work as intended, you can run it wherever you like... as long as it's in a users home directory 
-FLAGS="--bolt-path ${INFLUXDB}.influxdbv2/influxd.bolt --engine-path ${INFLUXDB}.influxdbv2/engine --http-bind-address 0.0.0.0:9999 --log-level debug --reporting-disabled"
+FLAGS="--bolt-path ${INFLUXDB}.influxdbv2/influxd.bolt --engine-path ${INFLUXDB}.influxdbv2/engine --http-bind-address 0.0.0.0:9999 --log-level debug --reporting-disabled --tls-cert=\"${CERTDIR}/${HOSTNAME}.crt\" --tls-key=\"${CERTDIR}/${HOSTNAME}.key\""
 /usr/local/bin/influxd $FLAGS
 
 ### FIXME: This is just an ugly hack to try and migrate the database from influx2-beta to influx2-rc
