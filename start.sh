b49398020e8b82f082c5d68671f28422c42fe977
Einar Lönn <el@sunet.se>
2020-11-05 14:02:27 +0100

    Weird, doesnt work from Telegraf but works in curl & chrome. Testing chain instead of cert in config...

--- start.sh
+++ start.sh
@@ -18,7 +18,7 @@ INFLUXDB="/var/lib/influxdb/"
 
 #FLAGS="--http-bind-address=0.0.0.0:9999 --log-level debug --reporting-disabled"
 # FIXME: I'd want it to run with something similar to below but it doesnt work as intended, you can run it wherever you like... as long as it's in a users home directory 
-FLAGS="--bolt-path ${INFLUXDB}.influxdbv2/influxd.bolt --engine-path ${INFLUXDB}.influxdbv2/engine --http-bind-address 0.0.0.0:8086 --log-level debug --reporting-disabled --tls-cert=${CERTDIR}/${HOSTNAME}.crt --tls-key=${CERTDIR}/${HOSTNAME}.key"
+FLAGS="--bolt-path ${INFLUXDB}.influxdbv2/influxd.bolt --engine-path ${INFLUXDB}.influxdbv2/engine --http-bind-address 0.0.0.0:8086 --log-level debug --reporting-disabled --tls-cert=${CERTDIR}/${HOSTNAME}-chain.crt --tls-key=${CERTDIR}/${HOSTNAME}.key"
 /usr/local/bin/influxd $FLAGS
 
 ### FIXME: This is just an ugly hack to try and migrate the database from influx2-beta to influx2-rc
@@ -26,6 +26,3 @@ FLAGS="--bolt-path ${INFLUXDB}.influxdbv2/influxd.bolt --engine-path ${INFLUXDB}
 #FLAGS="--bolt-path /home/influxdb/.influxdbv2_old/influxd.bolt --engine-path /home/influxdb/.influxdbv2_old/engine --http-bind-address=0.0.0.0:9999 --log-level debug --reporting-disabled "
 #runuser influxdb -c "/opt/local/bin/influxd_old $FLAGS &"
 #runuser influxdb -c "/opt/local/bin/influxd --http-bind-address=0.0.0.0:8046 --log-level debug --reporting-disabled"
-
-# FIXME: I'm not entirely sure why but whatever you do you'll end up with 2 databases, one in a semi-proper place (/var/lib/influxdb/.influxdbv2) and one in a static place (~/.influxdbv2).
-#runuser influxdb -c "/opt/local/bin/influxd $FLAGS "
