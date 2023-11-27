d4c6d3013737ea47fb3d84a47abf943b1f8fdf56
Jonas Hagström <jonas@sunet.se>
2023-11-27 11:37:54 +0100

    changed cert dir to change from acme-c to acme-d

--- start.sh
+++ start.sh
@@ -2,17 +2,17 @@
 
 printenv
 
-if [ "x${HOSTNAME}" = "x" ]; then
-   HOSTNAME="`hostname`"
+if [ "x${SERVICENAME}" = "x" ]; then
+   SERVICENAME="`servicename`"
 fi
 
 if [ "x${CERTDIR}" = "x" ]; then
-   CERTDIR="/etc/dehydrated/certs"
+   CERTDIR="/etc/letsencrypt/live/"
 fi
 
 # With influx2 out of alpha/beta you can now run in a proper location, some artifacts remain of "/home/influx" but they will be removed
 INFLUXDB="/var/lib/influxdb/"
 
 # Debug log is not as spammy as one would think and --reporting-disabled is a good idea to opt-out of "calling home"
-FLAGS="--bolt-path ${INFLUXDB}.influxdbv2/influxd.bolt --engine-path ${INFLUXDB}.influxdbv2/engine --http-bind-address 0.0.0.0:8086 --log-level debug --reporting-disabled --tls-cert=${CERTDIR}/${HOSTNAME}.crt --tls-key=${CERTDIR}/${HOSTNAME}.key"
+FLAGS="--bolt-path ${INFLUXDB}.influxdbv2/influxd.bolt --engine-path ${INFLUXDB}.influxdbv2/engine --http-bind-address 0.0.0.0:8086 --log-level debug --reporting-disabled --tls-cert=${CERTDIR}/${SERVICENAME}/cert.pem --tls-key=${CERTDIR}${SERVICENAME}/privkey.pem"
 /usr/local/bin/influxd $FLAGS
